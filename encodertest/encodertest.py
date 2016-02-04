0fe187c005eba22129ea047d7c1d3c52fc782835
Jacob Kingery <jacob.kingery@students.olin.edu>
2016-02-04 00:20:12 -0500

    tested encoder

--- encodertest/encodertest.py
+++ encodertest/encodertest.py
@@ -79,3 +79,19 @@ class encodertest:
             print "Could not send ENC_READ_REG vendor request."
         else:
             return ret
+
+
+def toWord(byteArray):
+    return int(byteArray[0]) + int(byteArray[1])*256
+
+myEncoderTest = encodertest()
+mask = 0x3FFF  # Sets first two bits (parity and error flag) to 0
+while True:
+    magBytes = myEncoderTest.enc_readReg(myEncoderTest.ENC_MAGNITUDE)
+    angBytes = myEncoderTest.enc_readReg(myEncoderTest.ENC_ANGLE_AFTER_ZERO_POS_ADDER)
+
+    magReading = toWord(magBytes)
+    angReading = toWord(angBytes)
+    print format(magReading, '016b'), float(magReading & mask) / mask * 360
+    print format(angReading, '016b'), float(angReading & mask) / mask * 360
+    print
